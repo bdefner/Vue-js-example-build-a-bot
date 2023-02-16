cdd5d9a4f8e6ce1329a917b72a755b60b2945630
Beppino Defner <hallo@deona.at>
2023-02-16 19:08:30 +0100

    Initiate project

---
+++ build-a-bot/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+    <HomePage />
+  </main>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HomePage,
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 100px;
+  box-sizing: border-box;
+  background-color: #333;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+main {
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  max-width: 1024px;
+  min-height: 300px;
+  background: whitesmoke;
+}
+</style>
